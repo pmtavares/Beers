a3eb2b6dfd9b5173141fbc3a0992affdcbbefed6
pmtavares <pedrotavares41@gmail.com>
2019-10-14 19:43:12 +0100

    Cart Controller, Summary Page, Date and time picker added.

--- Beer/Areas/Customer/Controllers/CartController.cs
+++ Beer/Areas/Customer/Controllers/CartController.cs
@@ -92,5 +92,99 @@ namespace Beer.Areas.Customer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Plus(int id)
+        {
+            var cart = await _db.ShoppingCart.Where(c=> c.Id == id).FirstOrDefaultAsync();
+            cart.Count += 1;
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        public async Task<IActionResult> Minus(int id)
+        {
+            var cart = await _db.ShoppingCart.Where(c => c.Id == id).FirstOrDefaultAsync();
+            if(cart.Count == 1)
+            {
+                _db.ShoppingCart.Remove(cart);
+                await _db.SaveChangesAsync();
+
+                var cnt = _db.ShoppingCart.Where(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.cntShoppingCart, cnt);
+
+            }
+            else
+            {
+                cart.Count -= 1;
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var cart = await _db.ShoppingCart.FirstOrDefaultAsync(c => c.Id == id);
+            _db.ShoppingCart.Remove(cart);
+            await _db.SaveChangesAsync();
+
+            var cnt = _db.ShoppingCart.Where(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            HttpContext.Session.SetInt32(SD.cntShoppingCart, cnt);
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        public async Task<IActionResult> Summary()
+        {
+            detailsCart = new OrderDetailsCart()
+            {
+                OrderHeader = new OrderHeader()
+            };
+
+            detailsCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            ApplicationUser applicationUser = await _db.ApplicationUser.Where(c => c.Id == claim.Value).FirstOrDefaultAsync();
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailsCart.ListCart = cart.ToList();
+            }
+
+
+            foreach (var list in detailsCart.ListCart)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                detailsCart.OrderHeader.OrderTotal = detailsCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+                list.MenuItem.Description = SD.ConvertToRawHtml(list.MenuItem.Description);              
+
+            }
+            detailsCart.OrderHeader.OriginalTotalOrder = detailsCart.OrderHeader.OrderTotal;
+            detailsCart.OrderHeader.PickUpName = applicationUser.Name;
+            detailsCart.OrderHeader.PickUpNumber = applicationUser.PhoneNumber;
+            detailsCart.OrderHeader.PickUpTime = DateTime.Now;
+
+
+
+            if (HttpContext.Session.GetString(SD.cntCouponCode) != null)
+            {
+                detailsCart.OrderHeader.CouponCode = HttpContext.Session.GetString(SD.cntCouponCode);
+
+                var couponFromDb = await _db.Coupon.Where(c => c.Name.ToLower() == detailsCart.OrderHeader.CouponCode.ToLower()).FirstOrDefaultAsync();
+
+                detailsCart.OrderHeader.OrderTotal = SD.DiscountedPrice(couponFromDb, detailsCart.OrderHeader.OriginalTotalOrder);
+            }
+
+            return View(detailsCart);
+        }
     }
 }
